41a13a666162150e43797919f23e69db2a56357f
oz-om <ozyoom@gmail.com>
2023-02-02 23:40:31 +0100

    fix

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export default function inputsValidation(input, config) {
-  return import(`.inputs/${input.name}.js`).then(module => module.default(input.value, config))
+  return import(`./inputs/${input.name}.js`).then(module => module.default(input.value, config))
 }
